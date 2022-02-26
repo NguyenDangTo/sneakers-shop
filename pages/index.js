14d1b2db7b1059030abceb40429be54d584ac2bf
NguyenDangTo <92070209+NguyenDangTo@users.noreply.github.com>
2022-02-26 14:54:38 +0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ productList, admin }) {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin
+          crossOrigin="true"
         ></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
